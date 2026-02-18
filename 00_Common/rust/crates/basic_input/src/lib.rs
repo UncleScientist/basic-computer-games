25a783bbdf5160b438ed21ff288d439d1e3ebf03
Uncle Scientist <unclescientist@gmail.com>
2026-02-18 07:36:54 -0500

    initial input line parsing

---
+++ 00_Common/rust/crates/basic_input/src/lib.rs
@@ -0,0 +1,26 @@
+mod parse;
+
+/*
+ * TODO:
+
+use std::io::{BufRead, Write};
+use std::str::FromStr;
+
+pub fn input<F: FromStr, P: AsRef<str>>(prompt: P) -> F {
+    loop {
+        print!("{}? ", prompt.as_ref());
+        let mut stdout = std::io::stdout().lock();
+        let _ = stdout.flush();
+
+        let mut buffer = String::new();
+        let stdin = std::io::stdin();
+        let mut handle = stdin.lock();
+        let _ = handle.read_line(&mut buffer);
+
+        if let Ok(result) = buffer.trim().to_string().parse::<F>() {
+            return result;
+        }
+        println!("?Re-enter");
+    }
+}
+*/
---
+++ 00_Common/rust/crates/basic_input/src/parse.rs
@@ -0,0 +1,72 @@
+fn parse<B: AsRef<str>>(buffer: B) -> Vec<String> {
+    let mut result = Vec::new();
+    let mut iter = buffer.as_ref().chars();
+    let mut in_quote = false;
+    let mut need_comma = false;
+
+    // for ch in iter {
+    let mut current = String::new();
+    while let Some(ch) = iter.next() {
+        match (ch, in_quote) {
+            (',', false) => {
+                result.push(current.trim().to_string());
+                current.clear();
+            }
+            ('"', true) => {
+                need_comma = true;
+                result.push(current.trim().to_string());
+                current.clear();
+            }
+            ('"', false) => {
+                in_quote = true;
+            }
+
+            (',', true) if need_comma => need_comma = false,
+            (',', true) => current.push(ch),
+
+            _ => current.push(ch),
+        }
+    }
+    if !current.is_empty() {
+        result.push(current.trim().to_string());
+    }
+
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_single_value() {
+        let result = parse("123");
+        assert_eq!("123".to_string(), result[0]);
+        assert_eq!(1, result.len());
+    }
+
+    #[test]
+    fn parse_two_values() {
+        let result = parse("123, 456");
+        assert_eq!("123".to_string(), result[0]);
+        assert_eq!("456".to_string(), result[1]);
+        assert_eq!(2, result.len());
+    }
+
+    #[test]
+    fn parse_quoted_value() {
+        let result = parse("123, \"456,789\", 10");
+        assert_eq!("123".to_string(), result[0]);
+        assert_eq!("456,789".to_string(), result[1]);
+        assert_eq!("10".to_string(), result[2]);
+        assert_eq!(3, result.len());
+    }
+
+    #[test]
+    fn parse_missing_closing_quote() {
+        let result = parse("123, \"456,789, 10");
+        assert_eq!("123".to_string(), result[0]);
+        assert_eq!("456,789, 10".to_string(), result[1]);
+        assert_eq!(2, result.len());
+    }
+}
