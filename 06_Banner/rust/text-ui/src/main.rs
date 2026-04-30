ca7633be3fce4bda194b9b2de0cccfeab04ecd4c
Uncle Scientist <unclescientist@gmail.com>
2026-04-30 08:42:53 -0400

    text UI done

--- 06_Banner/rust/text-ui/src/main.rs
+++ 06_Banner/rust/text-ui/src/main.rs
@@ -1,3 +1,56 @@
+use std::io::{BufRead, Write};
+
+use banner::Banner;
+
 fn main() {
-    println!("Hello, world!");
+    // Line 10
+    let horiz = loop {
+        let ret = prompt_for_input("HORIZONTAL");
+        if let Ok(val) = ret.parse::<usize>() {
+            break val;
+        }
+    };
+
+    // Line 20
+    let vert = loop {
+        let ret = prompt_for_input("VERTICAL");
+        if let Ok(val) = ret.parse::<usize>() {
+            break val;
+        }
+    };
+
+    // Line 22
+    let centered = prompt_for_input("CENTERED").as_str() > "P";
+
+    // Line 23
+    let elem = prompt_for_input("CHARACTER (TYPE 'ALL' IF YOU WANT CHARACTER BEING PRINTED)");
+
+    // Lines 29-30
+    let message = prompt_for_input("STATEMENT");
+
+    // Note: Ignoring line 35 as it has no effect
+
+    let centering = if centered {
+        (63.0 - 4.5 * vert as f32) / (elem.len() as f32 + 1.0)
+    } else {
+        0.0
+    } as usize;
+
+    let engine = Banner::new();
+    let iter = engine.banner(message, elem, horiz, vert);
+    for line in iter {
+        println!("{:<centering$}{line}", "");
+    }
+}
+
+fn prompt_for_input<S: AsRef<str>>(prompt: S) -> String {
+    print!("{}? ", prompt.as_ref());
+    let mut lock = std::io::stdout().lock();
+    let _ = lock.flush();
+
+    let mut buffer = String::new();
+    let stdin = std::io::stdin();
+    let mut handle = stdin.lock();
+    let _ = handle.read_line(&mut buffer);
+    buffer.trim().to_string()
 }
