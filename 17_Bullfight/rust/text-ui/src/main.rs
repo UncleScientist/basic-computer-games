1aa4f77b0bdc58cc98eb4c7f93e47cbaddfa6bbe
Uncle Scientist <unclescientist@gmail.com>
2026-09-09 07:54:57 -0400

    start of the text-based UI for bullfight

---
+++ 17_Bullfight/rust/text-ui/src/main.rs
@@ -0,0 +1,179 @@
+use std::io::{BufRead, Write};
+
+use bullfight::{Bullfight, KillMove, Outcome, PrepResult, RunOrRemain};
+
+fn main() {
+    println!("{:<34}BULL", "");
+    println!("{:<15}CREATIVE COMPUTING  MORRISTOWN, NEW JERSEY", "");
+
+    println!("\n\n");
+    let inst = prompt_for_string("Do you want instructions");
+    if !inst.eq_ignore_ascii_case("no") {
+        println!("HELLO, ALL YOU BLOODLOVERS AND AFICIONADOS.");
+        println!("HERE IS YOUR BIG CHANCE TO KILL A BULL.");
+        println!();
+        println!("ON EACH PASS OF THE BULL, YOU MAY TRY");
+        println!("0 - VERONICA (DANGEROUS INSIDE MOVE OF THE CAPE)");
+        println!("1 - LESS DANGEROUS OUTSIDE MOVE OF THE CAPE");
+        println!("2 - ORDINARY SWIRL OF THE CAPE.");
+        println!();
+        println!("INSTEAD OF THE ABOVE, YOU MAY TRY TO KILL THE BULL");
+        println!("ON ANY TURN: 4 (OVER THE HORNS), 5 (IN THE CHEST).");
+        println!("BUT IF I WERE YOU,");
+        println!("I WOULDN'T TRY IT BEFORE THE SEVENTH PASS.");
+        println!();
+        println!("THE CROWD WILL DETERMINE WHAT AWARD YOU DESERVE");
+        println!("(POSTHUMOUSLY IF NECESSARY).");
+        println!("THE BRAVER YOU ARE, THE BETTER THE AWARD YOU RECEIVE.");
+        println!();
+        println!("THE BETTER THE JOB THE PICADORES AND TOREADORES DO,");
+        println!("THE BETTER YOUR CHANCES ARE.");
+    }
+
+    println!("\n");
+
+    let mut bullfight = Bullfight::new();
+    let ability = bullfight.bull_ability();
+    println!("You have drawn a {ability} bull.");
+    match ability {
+        bullfight::Ability::Superb => println!("Good luck. You'll need it."),
+        bullfight::Ability::Good | bullfight::Ability::Fair | bullfight::Ability::Poor => {}
+        bullfight::Ability::Awful => println!("You're lucky."),
+    }
+
+    print_prep_result("Picadores", &bullfight.picadores);
+    print_prep_result("Toreadores", &bullfight.toreadores);
+
+    let mut player_dead = false;
+    loop {
+        let pass = bullfight.next_pass();
+        println!("Pass number {pass}");
+
+        if pass < 3 {
+            println!("The bull is charging at you! You are the matador--");
+            match prompt_for_yes_no("Do you want to kill the bull") {
+                YesOrNo::Yes => {
+                    let kill_prompt = prompt_for_kill_method();
+                    match kill_prompt {
+                        KillPrompt::KillMove(kill_move) => {
+                            let outcome = bullfight.kill_move(kill_move);
+                            match outcome {
+                                Outcome::PlayerDead => {
+                                    println!("The bull has gored you!");
+                                    player_dead = true;
+                                    break;
+                                }
+                                Outcome::StillAlive => {
+                                    println!("The bull has gored you!");
+                                    continue;
+                                }
+                                Outcome::BullDead => break,
+                                Outcome::Done | Outcome::Continue => unreachable!(),
+                            }
+                        }
+                        KillPrompt::Panic => {
+                            println!("You panicked. The bull gored you.");
+                            match bullfight.check_for_death() {
+                                Outcome::PlayerDead => {
+                                    player_dead = true;
+                                    break;
+                                }
+                                Outcome::StillAlive => {
+                                    println!("You are still alive.\n");
+                                    let run = prompt_for_yes_no("Do you run from the ring");
+                                    let decision = match run {
+                                        YesOrNo::No => {
+                                            println!("You are brave. Stupid, but brave.");
+                                            RunOrRemain::Remain
+                                        }
+                                        YesOrNo::Yes => {
+                                            println!("Coward");
+                                            RunOrRemain::Run
+                                        }
+                                    };
+                                    let outcome = bullfight.after_bull_charge(decision);
+                                    match outcome {
+                                        Outcome::Continue => continue,
+                                        Outcome::PlayerDead => {
+                                            player_dead = true;
+                                            break;
+                                        }
+                                        Outcome::StillAlive => continue,
+                                        Outcome::Done => break,
+                                        Outcome::BullDead => break,
+                                    }
+                                }
+                                _ => unreachable!(),
+                            }
+                        }
+                    }
+                }
+                YesOrNo::No => todo!(),
+            }
+        }
+    }
+
+    if player_dead {
+        println!("You are dead");
+    } else if bullfight.bull_killed() {
+        println!("bull killed .... tbd");
+        todo!();
+    } else {
+        println!("... outcome?");
+    }
+}
+
+fn print_prep_result(who: &str, prep: &PrepResult) {
+    println!("The {who} did a {} job.", prep.ability);
+    if prep.horses_killed > 0 {
+        println!("{} of the horses of the {who} killed.", prep.horses_killed);
+    }
+    println!("{} of the {who} killed.", prep.people_killed);
+}
+
+fn prompt_for_string<S: AsRef<str>>(prompt: S) -> String {
+    print!("{}? ", prompt.as_ref());
+    let mut lock = std::io::stdout().lock();
+    let _ = lock.flush();
+
+    let mut buffer = String::new();
+    let stdin = std::io::stdin();
+    let mut handle = stdin.lock();
+    let _ = handle.read_line(&mut buffer);
+    buffer.trim().to_string()
+}
+
+fn prompt_for_yes_no<S: AsRef<str>>(prompt: S) -> YesOrNo {
+    loop {
+        let answer = prompt_for_string(&prompt);
+        if answer.eq_ignore_ascii_case("yes") {
+            return YesOrNo::Yes;
+        }
+        if answer.eq_ignore_ascii_case("no") {
+            return YesOrNo::No;
+        }
+        println!("Incorrect answer - - please type 'yes' or 'no'.");
+    }
+}
+
+fn prompt_for_kill_method() -> KillPrompt {
+    println!("\nIt is the moment of truth\n");
+    let method = prompt_for_string("How do you try to kill the bull");
+    if method == "4" {
+        KillPrompt::KillMove(KillMove::OverTheHorns)
+    } else if method == "5" {
+        KillPrompt::KillMove(KillMove::InTheChest)
+    } else {
+        KillPrompt::Panic
+    }
+}
+
+enum KillPrompt {
+    KillMove(KillMove),
+    Panic,
+}
+
+enum YesOrNo {
+    Yes,
+    No,
+}
